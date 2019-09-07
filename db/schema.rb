5e835d506cdd5a17a6cd22d688f7821ce03c4efc
Johnty <johntysimon80@gmail.com>
2019-09-07 16:03:17 +0530

    Article model access using rails console

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_20_114947) do
+ActiveRecord::Schema.define(version: 2019_09_07_095633) do
 
-  create_table "comments", force: :cascade do |t|
-    t.string "description"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_comments_on_user_id"
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: :cascade do |t|
