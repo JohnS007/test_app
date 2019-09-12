73eb1a57cb2a6afea474ebee84daf88db96738ad
Johnty <johntysimon80@gmail.com>
2019-09-12 10:28:23 +0530

    Add edit update functionality and partial

--- app/controllers/zusers_controller.rb
+++ app/controllers/zusers_controller.rb
@@ -14,6 +14,20 @@ class ZusersController < ApplicationController
     end
   end
 
+  def edit
+    @zuser = Zuser.find(params[:id])
+  end
+
+  def update
+    @zuser = Zuser.find(params[:id])
+    if @zuser.update(zuser_params)
+      flash[:success] = "Your account was successfully updated"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+
   private
   def zuser_params
     params.require(:zuser).permit(:username, :email, :password)
